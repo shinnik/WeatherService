06f998f94c716641f3d832b2f920b2f745b17c02
Nikita Izrailev <izrailev@phystech.edu>
2019-05-25 13:12:10 +0300

    Updates

---
+++ precache-manifest.71a518db5fab58f992b270c7699b619e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408a1885ce66afd71e4fe4b7e9cf65bf",
+    "url": "/WeatherService/index.html"
+  },
+  {
+    "revision": "a0d7bb4147e701f6ff8c",
+    "url": "/WeatherService/static/css/main.caa654a4.chunk.css"
+  },
+  {
+    "revision": "cf0ad9732fc0426135d2",
+    "url": "/WeatherService/static/js/2.c6f7e6c9.chunk.js"
+  },
+  {
+    "revision": "a0d7bb4147e701f6ff8c",
+    "url": "/WeatherService/static/js/main.27934141.chunk.js"
+  },
+  {
+    "revision": "1affffc6d578c3b64e15",
+    "url": "/WeatherService/static/js/runtime~main.b2de1f62.js"
+  },
+  {
+    "revision": "cc6ab263dc10b893c41459a4898bef9f",
+    "url": "/WeatherService/static/media/arrow-down.cc6ab263.svg"
+  },
+  {
+    "revision": "d6858d4a5a9305720d10e045c43ddc11",
+    "url": "/WeatherService/static/media/erase.d6858d4a.svg"
+  },
+  {
+    "revision": "7195a9634e77464bfa443a8e866848db",
+    "url": "/WeatherService/static/media/info.7195a963.svg"
+  },
+  {
+    "revision": "e06d0a8458c59d059a48eb1e074c4a1f",
+    "url": "/WeatherService/static/media/invalid.e06d0a84.svg"
+  },
+  {
+    "revision": "4e914679a1e612cd37749232b1d13a11",
+    "url": "/WeatherService/static/media/location.4e914679.svg"
+  },
+  {
+    "revision": "2ffbaf4d3ec7739f240d7c104089a997",
+    "url": "/WeatherService/static/media/phone.2ffbaf4d.svg"
+  },
+  {
+    "revision": "2151cce6cc6aae7215da5f4763039955",
+    "url": "/WeatherService/static/media/valid.2151cce6.svg"
+  }
+]);
